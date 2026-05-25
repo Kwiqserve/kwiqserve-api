77b76d93bc4247ce597370027cf004dce33811bb
Immanuel Onum <immanuel.o.onum@gmail.com>
2026-05-25 16:08:27 +0100

    Add console log for request validation in validateRequest middleware

--- src/middleware/validateRequest.ts
+++ src/middleware/validateRequest.ts
@@ -5,6 +5,7 @@ import * as response from "../responses/index";
 
 const validate = (schema: AnySchema) => async (req: Request, res: Response, next: NextFunction) => {
     try {
+        console.log('validating request:', req.body)
         await schema.validate({
             body: req.body,
             query: req.query,
